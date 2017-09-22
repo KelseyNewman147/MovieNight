9b23d3f1ad5d0aab30286f5f49b546d4c9a11b96
Kelsey <kpnewman2013@gmail.com>
2017-09-21 21:30:38 -0400

    First commit

---
+++ src/main/java/com/Kelsey/demo/MovieNightApplication.java
@@ -0,0 +1,12 @@
+package com.Kelsey.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieNightApplication.class, args);
+	}
+}
